96181a9c64f485b37c0eb2efd353a31dc11faf2f
Bishal-sys <96173584+BishalDas-sys@users.noreply.github.com>
2022-02-02 16:04:02 +0530

    Updates

---
+++ precache-manifest.bd21f7873100bc6cd3dcd402ec8399ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31424326a8dfc871922abf709457a29a",
+    "url": "/CipherSchools/index.html"
+  },
+  {
+    "revision": "8ea7ba933d303aff2179",
+    "url": "/CipherSchools/static/css/main.f5b74edd.chunk.css"
+  },
+  {
+    "revision": "5c3c1742a40c4bf5a9fa",
+    "url": "/CipherSchools/static/js/2.84e277a6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/CipherSchools/static/js/2.84e277a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ea7ba933d303aff2179",
+    "url": "/CipherSchools/static/js/main.47ea606a.chunk.js"
+  },
+  {
+    "revision": "19b7f49d755753fe0e55",
+    "url": "/CipherSchools/static/js/runtime-main.a47d14e9.js"
+  }
+]);
